0f806185563f8619e70ae7a982ffa495efd74be9
Karl Brodowsky <bk1@gmx.net>
2016-03-02 21:01:20 +0100

    use captain

--- routes/captain_routes.rb
+++ routes/captain_routes.rb
@@ -1,58 +1,58 @@
 # encoding: UTF-8
 require_relative '../models/admin'
-require_relative '../models/user'
-require_relative '../views/admin/user/all'
+require_relative '../models/captain'
+require_relative '../views/admin/captain/all'
 require_relative '../views/forbidden_request'
 
-class UserRoutes < Cuba
+class CaptainRoutes < Cuba
   define do
     on get do
       on 'index' do
-        res.write forti(Views::Admin::User::Index, users:User.all)
+        res.write forti(Views::Admin::Captain::Index, captains:Captain.all)
       end
 
       on 'new' do
-        res.write forti(Views::Admin::User::New, user:User.new)
+        res.write forti(Views::Admin::Captain::New, captain:Captain.new)
       end
 
       on ':id' do |id|
-        usr=User[:id]
-        if usr
+        cpt=Captain[:id]
+        if cpt
           on root do
-            res.write forti(Views::Admin::User::Show, user:usr)
+            res.write forti(Views::Admin::Captain::Show, captain:cpt)
           end
           on 'edit' do
-            res.write forti(Views::Admin::User::Edit, user:usr)
+            res.write forti(Views::Admin::Captain::Edit, captain:cpt)
           end
         else
-          flash[:error]='Could not find user'
-          res.redirect '/admin/user/index'
+          flash[:error]='Could not find captain'
+          res.redirect '/admin/captain/index'
         end
       end
     end
 
-    on post, root, param('user') do |user|
-      usr = User.new(user)
-      if usr.save
-        res.redirect usr.show_path
+    on post, root, param('captain') do |captain|
+      cpt = Captain.new(captain)
+      if cpt.save
+        res.redirect cpt.show_path
       else
-        res.write forti(Views::Admin::User::New, user:usr)
+        res.write forti(Views::Admin::Captain::New, captain:cpt)
       end
     end
 
-    on put, ':id', param('user') do |id, user|
-      usr=User[id:id]
-      if usr
-        usr.set_fields(user, ['name', 'email'])
-        if usr.valid? && usr.save
-          res.write forti(Views::Admin::User::Show, user:usr)
+    on put, ':id', param('captain') do |id, captain|
+      cpt=Captain[id:id]
+      if cpt
+        cpt.set_fields(captain, ['name', 'email'])
+        if cpt.valid? && cpt.save
+          res.write forti(Views::Admin::Captain::Show, captain:cpt)
         else
-          flash[:error]="user could not be saved"
-          res.write forti(Views::Admin::User::Edit, user:usr)
+          flash[:error]="captain could not be saved"
+          res.write forti(Views::Admin::Captain::Edit, captain:cpt)
         end
       else
-        flash[:error]='Could not find user #{id}'
-        res.redirect '/admin/user/index'
+        flash[:error]='Could not find captain #{id}'
+        res.redirect '/admin/captain/index'
       end
     end
   end
