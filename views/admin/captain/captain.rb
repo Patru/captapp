0258c8f1b60e1a42911c482bead72c736eb205bc
Karl Brodowsky <bk1@gmx.net>
2016-03-02 21:16:00 +0100

    adapted

--- views/admin/captain/captain.rb
+++ views/admin/captain/captain.rb
@@ -3,13 +3,13 @@ require_relative '../../page'
 
 module Views
   module Admin
-    module User
-      class User < Page
-        needs :user
+    module Captain
+      class Captain < Page
+        needs :captain
         def form action, button='Create', method='post'
-          frm= Forme.form(user, {action: action, method: 'post'},
+          frm= Forme.form(captain, {action: action, method: 'post'},
               hidden_tags: [{_method: method}, csrf_token]) do |f|
-              f.inputs([:email, :name], legend: 'User data') do
+              f.inputs([:email, :name], legend: 'Captain data') do
                 f.input(:password, type: :password)
                 f.button(value: button, name: 'send')
               end
